ff6f4d51092ad2a1f19a07ad1a26eeeb2347a93e
Jake Lawrence <jake.lawrence24@gmail.com>
2016-12-27 19:06:08 -0500

    changed functionality of cli

--- slackshell.py
+++ slackshell.py
@@ -6,13 +6,15 @@ SLACK_TOKEN = os.environ['SLACK_TOKEN']
 sc = SlackClient(SLACK_TOKEN)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("intent", help="Determines what commands to execute. " +
-"Valid commands include:\n\tpost\nwip")
+parser.add_argument("--post", help="Channel where the message will be sent")
+parser.add_argument("--send", help="Channel where the message will be sent")
+parser.add_argument("--update", help="Channel where the message will be sent")
 parser.add_argument("-c","--channel", help="Channel where the message will be sent")
-parser.add_argument("-r","--receiver", help="Channel where the message will be sent")
+parser.add_argument("-r","--receiver", help="Recipent where the message will be sent")
+parser.add_argument("-a","--address", help="Address to where the message will be sent")
 parser.add_argument("-m", "--message", help="Input is the message that will be sent")
 parser.add_argument("-t", "--time", help="Timestamp of the message that wants to be updated")
-parser.add_argument("-u", "--update", help="Input is the message that will be sent")
+
 args = parser.parse_args()
 
 message_cache = open("cache/message_cache.txt", 'a')
@@ -33,18 +35,24 @@ def updateMessage(time,channel,message):
     print("f")
 
 def delegate(args):
-    #try:
-    if(args.intent == "post"):
-        e = "Error: Post must contain channel and message fields"
-        postMessage(args.channel,args.message)
-    if(args.intent == "send"):
-        e = "Error: Content must contain channel and message fields"
-        sendMessage(args.receiver, args.message)
-    if(args.intent == "update"):
+    if(args.post != None):
+        if(args.channel != None):
+            args.address = args.channel
+        try:
+            postMessage(args.address, args.post)
+        except:
+            print("Error: Invalid channel")
+    elif(args.send != None):
+        if(args.receiver != None):
+            args.address = args.receiver
+        try:
+            sendMessage(args.address, args.send)
+        except:
+            print("Error: Invalid recipent")
+    elif(args.update != None):
         e = "Error: Update must contain valid timestamp, channel, and message fields"
         sendMessage(args.time, args.channel, args.message)
-    #except:
-        #print(e)
+    return
 
 delegate(args)
 message_cache.close()
